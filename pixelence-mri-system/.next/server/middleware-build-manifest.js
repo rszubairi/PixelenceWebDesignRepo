ec1db9a84d2f344d8592bfb9bc953849cec218f8
User <user@example.com>
2026-02-16 19:15:18 +0800

    applify deployment changes

--- pixelence-mri-system/.next/server/middleware-build-manifest.js
+++ pixelence-mri-system/.next/server/middleware-build-manifest.js
@@ -28,6 +28,11 @@ self.__BUILD_MANIFEST = {
       "static/chunks/webpack.js",
       "static/chunks/main.js",
       "static/chunks/pages/settings/license.js"
+    ],
+    "/settings/system": [
+      "static/chunks/webpack.js",
+      "static/chunks/main.js",
+      "static/chunks/pages/settings/system.js"
     ]
   },
   "ampFirstPages": []
--- pixelence-mri-system/.next/static/chunks/webpack.js
+++ pixelence-mri-system/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "efb6722ef9c70a33"; }
+/******/ 		__webpack_require__.h = function() { return "f9d3ce325c0765b4"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
